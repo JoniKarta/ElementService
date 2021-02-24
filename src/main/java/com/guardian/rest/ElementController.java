8656242ca52261e26bda3a9c5378625fcf160c27
JoniKarta <Jkarta26@gmail.com>
2021-02-24 12:17:44 +0200

    added delete route for testing

--- src/main/java/com/guardian/rest/ElementController.java
+++ src/main/java/com/guardian/rest/ElementController.java
@@ -3,7 +3,6 @@ package com.guardian.rest;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
